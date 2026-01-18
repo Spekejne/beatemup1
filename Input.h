39452369bf5c5eea04c09eb697f7cdbe13b14473
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-18 22:20:24 +0100

    Update Input.h

--- Input.h
+++ Input.h
@@ -1,12 +1,16 @@
 #pragma once
 
-enum InputType { NONE, UP, DOWN, LEFT, RIGHT, X, Y };
+#include <SDL2/SDL.h>
+
+enum InputType { NONE, LEFT, RIGHT, UP, DOWN, X, Y };
 
 struct ComboBuffer {
-    InputType inputs[10];
-    float times[10];
+    InputType inputs[10]; // maks 10 ostatnich komend
+    float timestamps[10];
     int count;
+
+    void clear() { count = 0; }
 };
 
-void pushInput(ComboBuffer* buffer, InputType input, float deltaTime);
-void handleInput(class Player* player, SDL_Event& e);
+void pushInput(ComboBuffer* buffer, InputType input, float time);
+InputType getLastInput(const ComboBuffer* buffer);
