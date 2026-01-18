767ce325e50ca9765710898ea1a55678c3bec01e
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-18 23:11:47 +0100

    Update Stage.cpp

--- Stage.cpp
+++ Stage.cpp
@@ -1,19 +1,7 @@
 #include "Stage.h"
-#include <SDL2/SDL_image.h>
 
-Stage::Stage() : background(nullptr) {}
+void Stage::loadStage(const char* filename) {}
 
-void Stage::init(SDL_Renderer* renderer) {
-    SDL_Surface* surf = SDL_LoadBMP("assets/background.bmp");
-    background = SDL_CreateTextureFromSurface(renderer, surf);
-    SDL_FreeSurface(surf);
-}
+void Stage::render(SDL_Renderer* renderer) {}
 
-void Stage::update(float dt) {
-    // np. animacja tła
-}
-
-void Stage::render(SDL_Renderer* renderer) {
-    SDL_Rect dst = { 0, 0, 800, 600 };
-    SDL_RenderCopy(renderer, background, NULL, &dst);
-}
+bool Stage::isComplete(const Player& player) { return false; }
