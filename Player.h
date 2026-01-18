c9a0bc079affadb7e4f7fdaaf1ba826bc8bc1470
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-18 23:02:50 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,22 +1,28 @@
-#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
 #include <SDL2/SDL.h>
 #include "Input.h"
+#include "Enemy.h"
 
 class Player {
 public:
-    Player();
     void init(SDL_Renderer* renderer);
     void update(float dt);
     void render(SDL_Renderer* renderer);
-    void processCombo(const ComboBuffer& buffer);
+    void processCombo(const ComboBuffer& combo);
 
+    bool checkHit(const Enemy& enemy);
+    void onHit(int damage);
+
+    void moveLeft(float dt);
+    void moveRight(float dt);
+    void jump(float dt);
+    void crouch(float dt);
+
+private:
     int x, y;
     int health;
-private:
-    SDL_Texture* spriteIdle;
-    SDL_Texture* spriteWalk;
-    SDL_Texture* spriteAttackLight;
-    SDL_Texture* spriteAttackHeavy;
-
-    float speed;
 };
+
+#endif
