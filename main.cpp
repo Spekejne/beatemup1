c8c64694af108ef0ff689de7f8124d5a21e58d27
Spekejne <115970082+Spekejne@users.noreply.github.com>
2026-01-18 23:07:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,10 @@
 #include "Game.h"
-#include <SDL2/SDL.h>
-#include <iostream>
 
-int main(int argc, char** argv) {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
-        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-        return -1;
-    }
-
+int main(int argc, char* argv[]) {
     Game game;
+    if (!game.init()) return -1;
 
-    Uint32 lastTime = SDL_GetTicks();
-    while (game.isRunning()) {
-        Uint32 current = SDL_GetTicks();
-        float dt = (current - lastTime) / 1000.0f;
-        lastTime = current;
-
-        game.handleEvents();
-        game.handleInput(dt);
-        game.update(dt);
-        game.render();
-
-        SDL_Delay(16); // ~60 FPS
-    }
-
-    SDL_Quit();
+    game.run();
+    game.cleanup();
     return 0;
 }
